5fb80719ffa2064cd10499a6478411694c990d6d
Illumicone Dev Team <illumicone@illumicone.com>
2016-07-11 01:14:14 -0600

    Steps widget can now receive data via UDP.

--- src/StairWidget.cpp
+++ src/StairWidget.cpp
@@ -37,52 +37,19 @@ void StairWidget::init(bool generateSimulatedMeasurements)
     for (int i = 0; i < 5; ++i) {
         channels.push_back(make_shared<WidgetChannel>(i, this));
     }
+
+    if (!generateSimulatedMeasurements) {
+        startUdpRxThread();
+    }
 }
 
 
 bool StairWidget::moveData()
 {
-//    for (auto&& channel:channels) {
-//        switch (channel.number) {
-//            case 0:
-//                channel.velocity = 0;
-//                channel.prevPosition = channel.position;
-//                channel.isActive = 1;
-//                // position = 1 means stair is being stepped on
-//                channel.position = 1;
-//
-//                break;
-//
-//            case 1:
-//                channel.velocity = 0;
-//                channel.prevPosition = channel.position;
-//                channel.isActive = 1;
-//                // position = 1 means stair is being stepped on
-//                channel.position = 1;
-//
-//                break;
-//
-//            case 2:
-//                channel.velocity = 0;
-//                channel.prevPosition = channel.position;
-//                channel.isActive = 1;
-//                // position = 1 means stair is being stepped on
-//                channel.position = 1;
-//
-//                break;
-//
-//            case 3:
-//                channel.velocity = 0;
-//                channel.prevPosition = channel.position;
-//                channel.isActive = 1;
-//                channel.position = 1;
-//
-//                break;
-//
-//            default:
-//                return false;
-//        }
-//    }
+    if (!generateSimulatedMeasurements) {
+        return true;
+    }
+
     using namespace std::chrono;
 
     milliseconds epochMs = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
