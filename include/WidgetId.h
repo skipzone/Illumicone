eb4a1412d920b696c93bbbac11f7e2b886f64ea1
skipzone <ross271828@gmail.com>
2016-08-15 11:19:56 -0600

    Added copyright notice to all source and header files.

--- include/WidgetId.h
+++ include/WidgetId.h
@@ -1,3 +1,20 @@
+/*
+    This file is part of Illumicone.
+
+    Illumicone is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Illumicone is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Illumicone.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 /* Widget Id Assignment
@@ -11,7 +28,7 @@
  *  6:  Phyxx's TriObelisk (the triple rotary thing)
  *  7:  Cowboy's Box Theramin
  *  8:  Kayla's Plunger
- *  9:  unassigned
+ *  9:  Ross's FourPlay
  * 10:  unassigned
  * 11:  unassigned
  * 12:  unassigned
