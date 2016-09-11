6966be9a16425eb0c6a480c596c05b944910d946
Ross Butler <ross271828@gmail.com>
2016-09-11 13:05:42 -0600

    Updated widgetRcvr.cpp with version from Burning Man that supports FourPlay.

--- src/widgetRcvr.cpp
+++ src/widgetRcvr.cpp
@@ -1,20 +1,3 @@
-/*
-    This file is part of Illumicone.
-
-    Illumicone is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Illumicone is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Illumicone.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
@@ -431,6 +414,7 @@ int main(int argc, char** argv)
     openUdpPort(WidgetId::triObelisk);
     openUdpPort(WidgetId::boxTheramin);
     openUdpPort(WidgetId::plunger);
+    openUdpPort(WidgetId::fourPlay);
 
     configureRadio();
 
