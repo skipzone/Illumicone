38b42d8cf33f2f165259eac38aab0c6e56fb82c4
skipzone <ross271828@gmail.com>
2016-11-10 23:15:23 -0700

    Added plots for all three events.  Fixed up lineseries colors.  Eliminated rows for nonexistent widgets 0 and 11-15 from y matrix.  Added copyright headers.

--- database/setConsideredActiveByPattern.sql
+++ database/setConsideredActiveByPattern.sql
@@ -1,3 +1,27 @@
+/*
+    ----------------------------------------------------------------------------
+    This script sets the widget_packet.considered_active_by_pattern values.
+
+    Ross Butler  November 2016
+    ----------------------------------------------------------------------------
+
+    This file is part of Illumicone.
+
+    Illumicone is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Illumicone is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Illumicone.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 -- The thresholds used below are those that were in effect for Burning Man 2016
 -- and Idaho Decompression 2016.  They may not be correct for Element 11 2016.
 
@@ -65,3 +89,4 @@ UPDATE widget_packet
 SET considered_active_by_pattern = CASE payload_type_id WHEN 1 THEN is_active ELSE 0 END
 WHERE widget_id = 9
       AND considered_active_by_pattern IS NULL
+
