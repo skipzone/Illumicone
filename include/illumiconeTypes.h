a846919c5aa90eb739eef12a858021a6f45abb95
skipzone <ross271828@gmail.com>
2017-06-09 00:56:38 -0600

    Added localhost to illumiconeTypes.h for simulation mode.

--- include/illumiconeTypes.h
+++ include/illumiconeTypes.h
@@ -30,15 +30,19 @@
 
 #ifdef SIMULATION_MODE
 
+// localhost
+#define OPC_SERVER_ADDR "127.0.0.1"
 // Xanadu shop computer
-#define OPC_SERVER_ADDR "192.168.0.17"
+//#define OPC_SERVER_ADDR "192.168.0.17"
 // david's VM (14), ross's mac (12) at Xanadu
 //#define OPC_SERVER_ADDR "192.168.69.13"
 // david's home addr for the sever
 //#define OPC_SERVER_ADDR "192.168.0.10"
 
+// localhost
+static const std::string patconIpAddress = "127.0.0.1";
 // Xanadu shop computer
-static const std::string patconIpAddress = "192.168.0.17";
+//static const std::string patconIpAddress = "192.168.0.17";
 
 #else
 
