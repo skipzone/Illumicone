2d25d1741aa03d214ed315dbf0e4d48157ec1c9b
skipzone <ross271828@gmail.com>
2017-06-17 20:34:04 -0600

    Got Schroeder's Plaything working.  Added system and pitch bend handling.

--- Arduino/Widget_SchroedersPlaything/Widget_SchroedersPlaything.ino
+++ Arduino/Widget_SchroedersPlaything/Widget_SchroedersPlaything.ino
@@ -1,10 +1,10 @@
 /*****************************************************************
  *                                                               *
- * Ray's Bells Widget                                            *
+ * Schroeder's Plaything Widget                                  *
  *                                                               *
  * Platform:  Arduino Uno, Pro, Pro Mini                         *
  *                                                               *
- * by Ross Butler, August 2016                               )'( *
+ * by Ross Butler, June 2017                                 )'( *
  *                                                               *
  *****************************************************************/
 
@@ -37,8 +37,8 @@ MIDI_CREATE_DEFAULT_INSTANCE();
  * Widget Configuration *
  ************************/
 
-#define WIDGET_ID 5
-#define TX_INTERVAL_MS 200L
+#define WIDGET_ID 4   // change back to 5
+//#define TX_INTERVAL_MS 1000L
 //#define TX_FAILURE_LED_PIN 8
 
 
@@ -77,6 +77,12 @@ static bool gotNoteOff;
 static byte noteOffChannel;
 static byte noteOffPitch;
 static byte noteOffVelocity;
+static bool gotPitchBend;
+static byte pitchBendChannel;
+static byte pitchBend;
+static bool gotProgramChange;
+static byte programChangeChannel;
+static byte programChangeNumber;
 
 
 /******************
@@ -95,7 +101,7 @@ void handleNoteOn(byte in_channel, byte in_pitch, byte in_velocity)
   noteOnPitch = in_pitch;
   noteOnVelocity = in_velocity;
   gotNoteOn = true;
-      digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 
@@ -105,25 +111,41 @@ void handleNoteOff(byte in_channel, byte in_pitch, byte in_velocity)
   noteOffPitch = in_pitch;
   noteOffVelocity = in_velocity;
   gotNoteOff = true;
-      digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+
+void handlePitchBend(byte in_channel, byte in_pitchBend)
+{
+  pitchBendChannel = in_channel;
+  pitchBend = in_pitchBend;
+  gotPitchBend = true;
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+
+void handleProgramChange(byte in_channel, byte in_number)
+{
+  programChangeChannel = in_channel;
+  programChangeNumber = in_number;
+  gotProgramChange = true;
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 
 void setup()
 {
 
-//  configureRadio(radio, TX_PIPE_ADDRESS, TX_RETRY_DELAY_MULTIPLIER, TX_MAX_RETRIES, RF_POWER_LEVEL);
+  configureRadio(radio, TX_PIPE_ADDRESS, TX_RETRY_DELAY_MULTIPLIER, TX_MAX_RETRIES, RF_POWER_LEVEL);
   
   payload.widgetHeader.id = WIDGET_ID;
   payload.widgetHeader.isActive = true;
   payload.widgetHeader.channel = 0;
 
-  // Connect the handleNoteOn function to the library,
-  // so it is called upon reception of a NoteOn.
   MIDI.setHandleNoteOn(handleNoteOn);  // Put only the name of the function
-
-  // Do the same for NoteOffs
   MIDI.setHandleNoteOff(handleNoteOff);
+  MIDI.setHandlePitchBend(handlePitchBend);
+  MIDI.setHandleProgramChange(handleProgramChange);
 
   // Initiate MIDI communications, listen to all channels
   MIDI.begin(MIDI_CHANNEL_OMNI);
@@ -134,14 +156,14 @@ void setup()
 
 void loop() {
 
-//  static int32_t lastTxMs;
+  static int32_t lastTxMs;
 
-//  uint32_t now = millis();
+  uint32_t now = millis();
 
   // Call MIDI.read the fastest you can for real-time performance.
   MIDI.read();
 
-  if (gotNoteOn || gotNoteOff) {
+  if (gotNoteOn || gotNoteOff || gotPitchBend || gotProgramChange) {
 
     for (int i = 0; i < 15; ++i) {
       payload.measurements[i] = 0;
@@ -149,21 +171,33 @@ void loop() {
 
     if (gotNoteOn) {
       gotNoteOn = false;
-      payload.measurements[0] = noteOnChannel;
+      payload.measurements[0] = 1;
       payload.measurements[1] = noteOnPitch;
       payload.measurements[2] = noteOnVelocity;
     }
 
     if (gotNoteOff) {
       gotNoteOff = false;
-      payload.measurements[3] = noteOffChannel;
-      payload.measurements[4] = noteOffPitch;
-      payload.measurements[5] = noteOffVelocity;
+      payload.measurements[0] = 2;
+      payload.measurements[1] = noteOffPitch;
+      payload.measurements[2] = noteOffVelocity;
     }
 
-//  if (now - lastTxMs >= TX_INTERVAL_MS) {
+    if (gotPitchBend) {
+      gotPitchBend = false;
+      payload.measurements[0] = 3;
+      payload.measurements[1] = pitchBend;
+      payload.measurements[2] = 0;
+    }
 
-    if (!radio.write(&payload, sizeof(WidgetHeader) + sizeof(int16_t) * 6)) {
+    if (gotProgramChange) {
+      gotProgramChange = false;
+      payload.measurements[0] = 4;
+      payload.measurements[1] = programChangeNumber;
+      payload.measurements[2] = 0;
+    }
+
+    if (!radio.write(&payload, sizeof(WidgetHeader) + sizeof(int16_t) * 3)) {
 #ifdef TX_FAILURE_LED_PIN
       digitalWrite(TX_FAILURE_LED_PIN, HIGH);
 #endif
@@ -174,8 +208,14 @@ void loop() {
 #endif
     }
     
-//    lastTxMs = now;
   }
+
+//  if (now - lastTxMs >= TX_INTERVAL_MS) {
+//    lastTxMs = now;
+//    gotNoteOn = true;
+//    noteOnPitch = 1;
+//    noteOnVelocity = 2;
+//  }
 
 }
 
