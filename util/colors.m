b237f75ac7f667620d6e8f4b286140dd5b019ec3
skipzone <ross271828@gmail.com>
2018-02-20 10:10:20 -0700

    Added document for laminated setup card.  Added centering cables to diagram.

---
+++ util/colors.m
@@ -0,0 +1,24 @@
+% Most of these are non-standard (for MATLAB) colors normalized to MATLAB values.
+% (source:  http://kb.iu.edu/data/aetf.html)
+rgbOrange               = [255 140   0] ./ 255;
+rgbBlueViolet           = [138  43 226] ./ 255;
+rgbDarkMagenta          = [128   0 128] ./ 255;
+rgbDarkRed              = [128   0   0] ./ 255;
+rgbDarkGreen            = [  0 128   0] ./ 255;
+rgbDarkBlue             = [  0   0 128] ./ 255;
+rgbDarkGoldenrod        = [184 134  11] ./ 255;
+rgbDeepPink             = [255  20 147] ./ 255;
+rgbMediumSpringGreen    = [  0 250 154] ./ 255;
+rgbOliveDrab            = [107 142  35] ./ 255;
+rgbSeaGreen             = [ 46 139  87] ./ 255;
+rgbBrown                = [165  42  42] ./ 255;
+rgbChocolate            = [210 105  30] ./ 255;
+rgbChartreuse           = [127 255   0] ./ 255;
+rgbCadetBlue            = [ 95 158 160] ./ 255;
+rgbAquamarine           = [127 255 212] ./ 255;
+rgbCornflowerBlue       = [100 149 237] ./ 255;
+rgbTeal                 = [  0 128 128] ./ 255;
+rgbRed                  = [255   0   0] ./ 255;
+rgbBlue                 = [  0   0 255] ./ 255;
+rgbGray                 = [128 128 128] ./ 255;
+rgbDarkGray             = [ 64  64  64] ./ 255;
