1ee5130df7f1f0ceb15e837d4c05fe51bad4bfe6
skipzone <ross271828@gmail.com>
2019-02-24 17:38:04 -0700

    Adding widgetPlayback.py.  Reads widget data logs and prints what it would send, with correct timing (i.e., delays between messages).  Doesn't actually send data to patternController yet.

---
+++ sbin/widgetPlayback.py
@@ -0,0 +1,177 @@
+#! /usr/bin/python
+
+'''
+    ----------------------------------------------------------------------------
+    This program plays back widgetRcvr data logs, recreating and sending widget
+    data messages to patternController.
+
+    Ross Butler  February 2019
+    ----------------------------------------------------------------------------
+
+    This file is part of Illumicone.
+
+    Illumicone is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Illumicone is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Illumicone.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+from datetime import datetime, timedelta
+import os
+import re
+import sys
+from time import sleep
+
+
+lineCount = 0
+
+lastTimestamp = None
+
+
+def waitUntilTimestamp(timestamp):
+
+    global lastTimestamp
+
+    try:
+        thisTimestamp = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
+        if lastTimestamp:
+            interval = thisTimestamp - lastTimestamp
+            intervalS = interval.days * 3600 * 24 + interval.seconds + interval.microseconds / 1000000.0
+            if (intervalS > 0):
+                sleep(intervalS)
+
+        lastTimestamp = thisTimestamp
+
+    except ValueError as e:
+        sys.stderr.write('Invalid timestamp "{0} at line {1}.  {2}\n'.format(timestamp, lineCount, e))
+
+
+def sendPv(widgetData):
+    waitUntilTimestamp(widgetData['timestamp'])
+    print('Sending pv data:  {0}'.format(widgetData))
+
+
+def sendMvec(widgetData):
+    waitUntilTimestamp(widgetData['timestamp'])
+    print('Sending mvec data:  {0}'.format(widgetData))
+
+
+def sendCustom(widgetData):
+    waitUntilTimestamp(widgetData['timestamp'])
+    print('Sending custom data:  {0}'.format(widgetData))
+
+
+def processLogFile(logFileName):
+
+    global lineCount
+
+    # 2016-09-01 23:58:31.124:
+    timestampPattern = r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}):  '
+
+    # 2016-09-01 23:58:31.124:  pv: id=10 a=0 ch=0 p=178 v=0
+    pvPayloadPattern = timestampPattern + r'pv: id=(\d+) a=(\d+) ch=(\d+) p=(-?\d+) v=(-?\d+)$'
+
+    # 2016-09-01 23:58:31.124:  mvec: id=7 a=0 ch=0 n=3 1 2 3
+    ##mvecPayloadPattern = r'mvec: id=(\d+) a=(\d+) ch=(\d+) n=(\d+)(\s+-?\d+)+$'
+    mvecPayloadPattern = timestampPattern + r'mvec: id=(\d+) a=(\d+) ch=(\d+) n=(\d+)((?:\s+-?\d+)+)$'
+
+    # 2016-09-01 23:58:31.124:  custom: id=7 a=0 ch=0 bufLen=3 0x01 0x02 0x03
+    customPayloadPattern = timestampPattern + r'custom: id=(\d+) a=(\d+) ch=(\d+) bufLen=(\d+)((?:\s+0x[0-9a-fA-F]{2})+)$'
+
+    lineCount = 0
+
+    try:
+        with open(logFileName, 'r') as f:
+            for line in f:
+                if lineCount % 100 == 0:
+                    print('Processed {0} lines.'.format(lineCount))
+                lineCount += 1
+                line = line.rstrip()
+
+                m = re.search(pvPayloadPattern, line)
+                if m is not None:
+                    widgetData = {
+                        'timestamp' : m.group(1),
+                        'widgetId' : m.group(2),
+                        'isActive' : m.group(3),
+                        'channel' : m.group(4),
+                        'position' : m.group(5),
+                        'velocity' : m.group(6) }
+                    sendPv(widgetData)
+                    next
+
+                m = re.search(mvecPayloadPattern, line)
+                if m is not None:
+                    numMeasurements = int(m.group(5))
+                    measurements = [int(i) for i in m.group(6).split()]
+                    if (len(measurements) == numMeasurements):
+                        widgetData = {
+                            'timestamp' : m.group(1),
+                            'widgetId' : m.group(2),
+                            'isActive' : m.group(3),
+                            'channel' : m.group(4),
+                            'numMeasurements' : int(m.group(5)),
+                            'measurements' : measurements }
+                        sendMvec(widgetData)
+                    else:
+                        sys.stderr.write('measurement count inconsistency at line {0}\n'.format(lineCount))
+                    next
+
+                m = re.search(customPayloadPattern, line)
+                if m is not None:
+                    payloadLength = int(m.group(5))
+                    hexBytes = [i for i in m.group(6).split()]
+                    if (len(hexBytes) / 4 == payloadLength):
+                        widgetData = {
+                            'timestamp' : m.group(1),
+                            'widgetId' : m.group(2),
+                            'isActive' : m.group(3),
+                            'channel' : m.group(4),
+                            'payloadLength' : int(m.group(5)),
+                            'hexBytes' : hexBytes }
+                        sendCustom(widgetData)
+                    else:
+                        sys.stderr.write('payload length inconsistency at line {0}\n'.format(lineCount))
+                    next
+
+                # If we're here then the line was unrecognized, so ignore it.
+
+    except IOError as e:
+        sys.stderr.write('An error occurred with log file {0}.  {1}\n'.format(logFileName, e))
+
+    return
+
+
+def usage():
+    print('Usage:  widgetPlayback.py data_log_file_name')
+    return
+
+
+def main(argv):
+
+    if len(argv) <> 2:
+        usage()
+        return 2
+
+    inputFileName = argv[1]
+    if not os.path.exists(inputFileName):
+        sys.stderr.write('File {0} does not exist.\n'.format(inputFileName))
+        return 1
+
+    processLogFile(inputFileName)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+
