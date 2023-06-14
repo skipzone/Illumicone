82df6f3cde0dcdde46ef36fb120ca5737eca0f09
ross <ross271828@gmail.com>
2023-06-13 23:30:55 -0600

    Updated Eye radio config (250 Kbps, low power, ch. 97).

--- Arduino/Widget_Eye/Widget_Eye.ino
+++ Arduino/Widget_Eye/Widget_Eye.ino
@@ -60,7 +60,7 @@
 
 // Possible data rates are RF24_250KBPS, RF24_1MBPS, or RF24_2MBPS.  (2 Mbps
 // works with genuine Nordic Semiconductor chips only, not the counterfeits.)
-#define DATA_RATE RF24_1MBPS
+#define DATA_RATE RF24_250KBPS
 
 // Valid CRC length values are RF24_CRC_8, RF24_CRC_16, and RF24_CRC_DISABLED
 #define CRC_LENGTH RF24_CRC_16
@@ -72,7 +72,7 @@
 #define RF_CHANNEL 97
 
 // RF24_PA_MIN = -18 dBm, RF24_PA_LOW = -12 dBm, RF24_PA_HIGH = -6 dBm, RF24_PA_MAX = 0 dBm
-#define RF_POWER_LEVEL RF24_PA_MAX
+#define RF_POWER_LEVEL RF24_PA_LOW
 
 
 /***********
@@ -159,4 +159,3 @@ void loop() {
   }
 
 }
-
